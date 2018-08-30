7fb85d57def63f3012279423477fa8154655a95d
Florent Heyraud <florentheyraud.pro@gmail.com>
2018-08-30 10:42:29 +0200

    front beginning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import './style/App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Signup from './components/Signup/Signup'
+import Login from './components/Login/Login'
+
 
 class App extends Component {
-  
+
   render() {
 
     return (
+      <BrowserRouter>
       <div className="App">
-        <h1>Super app</h1>
+        <Navbar/>
+          <Route exact path="/signup" component={ Signup }/>
+          <Route exact path="/login" component={ Login }/>
       </div>
+      </BrowserRouter>
     );
   }
 }
